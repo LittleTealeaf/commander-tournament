c42e9e7771e139bd9ed938c2f5746e28509e7ed9
Thomas Kwashnak <ShintaoMonk@gmail.com>
2026-03-11 12:28:35 -0400

    chore: Color Enhancements (#15)
    * chore: color changes
    
    * reorder colors

--- edh_tourn/src/player/color.rs
+++ edh_tourn/src/player/color.rs
@@ -1,4 +1,5 @@
 use core::fmt::Display;
+use core::ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign};
 
 use itertools::Itertools;
 
@@ -19,12 +20,23 @@ use itertools::Itertools;
 pub struct ColorIdentity(pub(crate) u8);
 
 impl ColorIdentity {
+    #[allow(clippy::cast_possible_truncation, clippy::indexing_slicing)]
+    const IDENTITIES: [Self; 32] = {
+        let mut arr = [Self(0); 32];
+        let mut i = 0;
+        while i < 32 {
+            arr[i] = Self(i as u8);
+            i += 1;
+        }
+        arr
+    };
+
     #[must_use]
     pub const fn is_colorless(&self) -> bool {
         self.0 == 0
     }
 
-    pub fn to_colors(&self) -> impl Iterator<Item = MtgColor> {
+    pub fn into_colors(&self) -> impl Iterator<Item = MtgColor> {
         MtgColor::COLORS
             .into_iter()
             .filter(|color| self.has_color(*color))
@@ -32,16 +44,16 @@ impl ColorIdentity {
 
     #[must_use]
     pub const fn has_color(&self, color: MtgColor) -> bool {
-        (self.0 & color.to_identity_number()) == color.to_identity_number()
+        (self.0 & (color as u8)) == (color as u8)
     }
 
     pub const fn add_color(&mut self, color: MtgColor) {
-        self.0 |= color.to_identity_number();
+        self.0 |= color as u8;
     }
 
     pub const fn remove_color(&mut self, color: MtgColor) {
         if self.has_color(color) {
-            self.0 -= color.to_identity_number();
+            self.0 -= color as u8;
         }
     }
 }
@@ -95,9 +107,43 @@ impl FromIterator<MtgColor> for ColorIdentity {
         Self(
             iter.into_iter()
                 .unique()
-                .map(MtgColor::to_identity_number)
+                .map(|color| color as u8)
                 .sum::<u8>(),
         )
+    }
+}
+
+impl From<MtgColor> for ColorIdentity {
+    fn from(value: MtgColor) -> Self {
+        Self(value as u8)
+    }
+}
+
+impl BitAnd for ColorIdentity {
+    type Output = Self;
+
+    fn bitand(self, rhs: Self) -> Self::Output {
+        Self(self.0 & rhs.0)
+    }
+}
+
+impl BitOr for ColorIdentity {
+    type Output = Self;
+
+    fn bitor(self, rhs: Self) -> Self::Output {
+        Self(self.0 | rhs.0)
+    }
+}
+
+impl BitAndAssign for ColorIdentity {
+    fn bitand_assign(&mut self, rhs: Self) {
+        self.0 &= rhs.0;
+    }
+}
+
+impl BitOrAssign for ColorIdentity {
+    fn bitor_assign(&mut self, rhs: Self) {
+        self.0 |= rhs.0;
     }
 }
 
@@ -106,15 +152,15 @@ impl FromIterator<MtgColor> for ColorIdentity {
 )]
 pub enum MtgColor {
     #[serde(rename = "w", alias = "White")]
-    White,
+    White = 1,
     #[serde(rename = "u", alias = "Blue")]
-    Blue,
-    #[serde(rename = "g", alias = "Green")]
-    Green,
-    #[serde(rename = "r", alias = "Red")]
-    Red,
+    Blue = 2,
     #[serde(rename = "b", alias = "Black")]
-    Black,
+    Black = 4,
+    #[serde(rename = "r", alias = "Red")]
+    Red = 8,
+    #[serde(rename = "g", alias = "Green")]
+    Green = 16,
 }
 
 impl MtgColor {
@@ -130,27 +176,19 @@ impl MtgColor {
             Self::Black => "B",
         }
     }
-
-    const fn to_identity_number(self) -> u8 {
-        match self {
-            Self::White => 1,
-            Self::Blue => 1 << 1,
-            Self::Black => 1 << 2,
-            Self::Red => 1 << 3,
-            Self::Green => 1 << 4,
-        }
-    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashSet;
+
     use super::*;
 
     #[test]
     fn color_conversions() {
         for i in 0..32 {
             let color = ColorIdentity(i);
-            let values = color.to_colors().collect_vec();
+            let values = color.into_colors().collect_vec();
             let new_color: ColorIdentity = values.into_iter().collect();
             assert_eq!(color, new_color);
         }
@@ -164,5 +202,89 @@ mod tests {
 
         i.add_color(MtgColor::White);
         assert!(i.has_color(MtgColor::White));
+    }
+
+    #[test]
+    fn identities_are_all_colors() {
+        for identity in ColorIdentity::IDENTITIES {
+            assert_ne!("Unknown", identity.to_string());
+        }
+    }
+
+    #[test]
+    fn identities_are_all_different() {
+        let mut seen = HashSet::new();
+        for identity in ColorIdentity::IDENTITIES {
+            assert!(
+                seen.insert(identity),
+                "Duplicate identity found: {identity}"
+            );
+        }
+    }
+
+    #[test]
+    fn color_matches_identity() {
+        for color in MtgColor::COLORS {
+            let identity: ColorIdentity = color.into();
+            assert!(identity.has_color(color));
+        }
+    }
+
+    #[test]
+    fn color_bitand() {
+        let white_blue = ColorIdentity::from_iter([MtgColor::White, MtgColor::Blue]);
+        let blue_green = ColorIdentity::from_iter([MtgColor::Blue, MtgColor::Green]);
+        let white_green = ColorIdentity::from_iter([MtgColor::White, MtgColor::Green]);
+
+        assert_eq!(white_blue & blue_green, ColorIdentity::from(MtgColor::Blue));
+        assert_eq!(
+            white_blue & white_green,
+            ColorIdentity::from(MtgColor::White)
+        );
+        assert_eq!(
+            blue_green & white_green,
+            ColorIdentity::from(MtgColor::Green)
+        );
+    }
+
+    #[test]
+    fn color_bitor() {
+        let white_blue = ColorIdentity::from_iter([MtgColor::White, MtgColor::Blue]);
+        let blue_green = ColorIdentity::from_iter([MtgColor::Blue, MtgColor::Green]);
+        let white_green = ColorIdentity::from_iter([MtgColor::White, MtgColor::Green]);
+
+        assert_eq!(
+            white_blue | blue_green,
+            ColorIdentity::from_iter([MtgColor::White, MtgColor::Blue, MtgColor::Green])
+        );
+        assert_eq!(
+            white_blue | white_green,
+            ColorIdentity::from_iter([MtgColor::White, MtgColor::Blue, MtgColor::Green])
+        );
+        assert_eq!(
+            blue_green | white_green,
+            ColorIdentity::from_iter([MtgColor::White, MtgColor::Blue, MtgColor::Green])
+        );
+    }
+
+    #[test]
+    fn color_bitand_assign() {
+        let mut white_blue = ColorIdentity::from_iter([MtgColor::White, MtgColor::Blue]);
+        let blue_green = ColorIdentity::from_iter([MtgColor::Blue, MtgColor::Green]);
+
+        white_blue &= blue_green;
+        assert_eq!(white_blue, ColorIdentity::from(MtgColor::Blue));
+    }
+
+    #[test]
+    fn color_bitor_assign() {
+        let mut white_blue = ColorIdentity::from_iter([MtgColor::White, MtgColor::Blue]);
+        let blue_green = ColorIdentity::from_iter([MtgColor::Blue, MtgColor::Green]);
+
+        white_blue |= blue_green;
+        assert_eq!(
+            white_blue,
+            ColorIdentity::from_iter([MtgColor::White, MtgColor::Blue, MtgColor::Green])
+        );
     }
 }
