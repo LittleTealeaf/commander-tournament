e3d5ee956f558a17177f8c7d81d4a2285e10c03b
Thomas Kwashnak <thomaskwashnak@gmail.com>
2026-03-22 11:28:51 -0400

    move res to assets

--- app/src/style.rs
+++ app/src/style.rs
@@ -3,22 +3,22 @@ use iced::Font;
 pub const JETBRAINS_MONO: &str = "JetBrains Mono";
 
 pub const FONT_BYTES: [&[u8]; 16] = [
-    include_bytes!("../res/fonts/JetBrainsMono-Bold.ttf"),
-    include_bytes!("../res/fonts/JetBrainsMono-BoldItalic.ttf"),
-    include_bytes!("../res/fonts/JetBrainsMono-ExtraBold.ttf"),
-    include_bytes!("../res/fonts/JetBrainsMono-ExtraBoldItalic.ttf"),
-    include_bytes!("../res/fonts/JetBrainsMono-ExtraLight.ttf"),
-    include_bytes!("../res/fonts/JetBrainsMono-ExtraLightItalic.ttf"),
-    include_bytes!("../res/fonts/JetBrainsMono-Italic.ttf"),
-    include_bytes!("../res/fonts/JetBrainsMono-Light.ttf"),
-    include_bytes!("../res/fonts/JetBrainsMono-LightItalic.ttf"),
-    include_bytes!("../res/fonts/JetBrainsMono-Medium.ttf"),
-    include_bytes!("../res/fonts/JetBrainsMono-MediumItalic.ttf"),
-    include_bytes!("../res/fonts/JetBrainsMono-Regular.ttf"),
-    include_bytes!("../res/fonts/JetBrainsMono-SemiBold.ttf"),
-    include_bytes!("../res/fonts/JetBrainsMono-SemiBoldItalic.ttf"),
-    include_bytes!("../res/fonts/JetBrainsMono-Thin.ttf"),
-    include_bytes!("../res/fonts/JetBrainsMono-ThinItalic.ttf"),
+    include_bytes!("../assets/fonts/JetBrainsMono-Bold.ttf"),
+    include_bytes!("../assets/fonts/JetBrainsMono-BoldItalic.ttf"),
+    include_bytes!("../assets/fonts/JetBrainsMono-ExtraBold.ttf"),
+    include_bytes!("../assets/fonts/JetBrainsMono-ExtraBoldItalic.ttf"),
+    include_bytes!("../assets/fonts/JetBrainsMono-ExtraLight.ttf"),
+    include_bytes!("../assets/fonts/JetBrainsMono-ExtraLightItalic.ttf"),
+    include_bytes!("../assets/fonts/JetBrainsMono-Italic.ttf"),
+    include_bytes!("../assets/fonts/JetBrainsMono-Light.ttf"),
+    include_bytes!("../assets/fonts/JetBrainsMono-LightItalic.ttf"),
+    include_bytes!("../assets/fonts/JetBrainsMono-Medium.ttf"),
+    include_bytes!("../assets/fonts/JetBrainsMono-MediumItalic.ttf"),
+    include_bytes!("../assets/fonts/JetBrainsMono-Regular.ttf"),
+    include_bytes!("../assets/fonts/JetBrainsMono-SemiBold.ttf"),
+    include_bytes!("../assets/fonts/JetBrainsMono-SemiBoldItalic.ttf"),
+    include_bytes!("../assets/fonts/JetBrainsMono-Thin.ttf"),
+    include_bytes!("../assets/fonts/JetBrainsMono-ThinItalic.ttf"),
 ];
 
 #[must_use]
