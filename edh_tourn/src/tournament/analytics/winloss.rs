f074fc0aed025ab2b5e2abcc8f2b7584b736f06b
Thomas Kwashnak <thomaskwashnak@gmail.com>
2026-03-28 10:23:24 -0400

    cleanup

--- edh_tourn/src/tournament/analytics/winloss.rs
+++ edh_tourn/src/tournament/analytics/winloss.rs
@@ -1,16 +1,51 @@
 use std::collections::HashMap;
 
-use itertools::{Itertools, chain};
+use itertools::Itertools;
 
 use crate::{
     analytics::winloss::MatchPerformance,
     error::TournamentError,
+    game::record::GameRecord,
     player::{
         RegisteredPlayer,
         color::{ColorIdentity, MtgColor},
     },
     tournament::analytics::Analytics,
 };
+
+fn game_match_perfs<F>(game: &GameRecord, check: F) -> impl Iterator<Item = (u32, MatchPerformance)>
+where
+    F: Fn(u32) -> bool,
+{
+    let winner = game.winner();
+    let [loser_a, loser_b, loser_c] = game.losers();
+
+    [
+        check(winner).then_some([
+            (loser_a, MatchPerformance::WIN),
+            (loser_b, MatchPerformance::WIN),
+            (loser_c, MatchPerformance::WIN),
+        ]),
+        check(loser_a).then_some([
+            (winner, MatchPerformance::LOSS),
+            (loser_b, MatchPerformance::DRAW),
+            (loser_c, MatchPerformance::DRAW),
+        ]),
+        check(loser_b).then_some([
+            (winner, MatchPerformance::LOSS),
+            (loser_a, MatchPerformance::DRAW),
+            (loser_c, MatchPerformance::DRAW),
+        ]),
+        check(loser_c).then_some([
+            (winner, MatchPerformance::LOSS),
+            (loser_a, MatchPerformance::DRAW),
+            (loser_b, MatchPerformance::DRAW),
+        ]),
+    ]
+    .into_iter()
+    .flatten()
+    .flatten()
+}
 
 impl<'a> Analytics<'a> {
     fn player_get_all_players_match_performance(
@@ -23,35 +58,7 @@ impl<'a> Analytics<'a> {
         Ok(self
             .tourn
             .get_player_games(id)?
-            .flat_map(|game| {
-                let winner = game.winner();
-                let [loser_a, loser_b, loser_c] = game.losers();
-
-                [
-                    (winner == id).then_some([
-                        (loser_a, MatchPerformance::WIN),
-                        (loser_b, MatchPerformance::WIN),
-                        (loser_c, MatchPerformance::WIN),
-                    ]),
-                    (loser_a == id).then_some([
-                        (winner, MatchPerformance::LOSS),
-                        (loser_b, MatchPerformance::DRAW),
-                        (loser_c, MatchPerformance::DRAW),
-                    ]),
-                    (loser_b == id).then_some([
-                        (winner, MatchPerformance::LOSS),
-                        (loser_a, MatchPerformance::DRAW),
-                        (loser_c, MatchPerformance::DRAW),
-                    ]),
-                    (loser_c == id).then_some([
-                        (winner, MatchPerformance::LOSS),
-                        (loser_a, MatchPerformance::DRAW),
-                        (loser_b, MatchPerformance::DRAW),
-                    ]),
-                ]
-            })
-            .flatten()
-            .flatten()
+            .flat_map(|game| game_match_perfs(game, |i| i == id))
             .into_grouping_map()
             .sum()
             .into_iter()
@@ -103,46 +110,20 @@ impl Analytics<'_> {
         self,
         identity: ColorIdentity,
     ) -> HashMap<ColorIdentity, MatchPerformance> {
-        let identity_map = self
-            .tourn
-            .get_registered_players()
-            .map(|player| (player.id(), player.info().color_identity()))
-            .collect::<HashMap<_, _>>();
-
         self.tourn
             .games()
             .iter()
             .flat_map(|game| {
-                let winner = identity_map.get(&game.winner()).copied();
-                let [loser_a, loser_b, loser_c] =
-                    game.losers().map(|id| identity_map.get(&id).copied());
-
-                [
-                    (winner == Some(identity)).then_some([
-                        (loser_a, MatchPerformance::WIN),
-                        (loser_b, MatchPerformance::WIN),
-                        (loser_c, MatchPerformance::WIN),
-                    ]),
-                    (loser_a == Some(identity)).then_some([
-                        (winner, MatchPerformance::LOSS),
-                        (loser_b, MatchPerformance::DRAW),
-                        (loser_c, MatchPerformance::DRAW),
-                    ]),
-                    (loser_b == Some(identity)).then_some([
-                        (winner, MatchPerformance::LOSS),
-                        (loser_a, MatchPerformance::DRAW),
-                        (loser_c, MatchPerformance::DRAW),
-                    ]),
-                    (loser_c == Some(identity)).then_some([
-                        (winner, MatchPerformance::LOSS),
-                        (loser_a, MatchPerformance::DRAW),
-                        (loser_b, MatchPerformance::DRAW),
-                    ]),
-                ]
+                game_match_perfs(game, |id| {
+                    let Some(info) = self.tourn.get_player_info(&id) else {
+                        return false;
+                    };
+                    info.color_identity() == identity
+                })
             })
-            .flatten()
-            .flatten()
-            .filter_map(|(ident, perf)| Some((ident?, perf)))
+            .filter_map(|(id, perf)| {
+                Some((self.tourn.get_player_info(&id)?.color_identity(), perf))
+            })
             .into_grouping_map()
             .sum()
     }
@@ -166,43 +147,19 @@ impl Analytics<'_> {
         self,
         color: MtgColor,
     ) -> HashMap<MtgColor, MatchPerformance> {
-        let get_identity = |id: u32| -> ColorIdentity {
-            let Some(info) = self.tourn.get_player_info(&id) else {
-                return ColorIdentity::COLORLESS;
-            };
-            info.color_identity()
-        };
-
         self.tourn
             .games()
             .iter()
             .flat_map(|game| {
-                let winner = get_identity(game.winner());
-                let [loser_a, loser_b, loser_c] = game.losers().map(&get_identity);
-
-                chain!(
-                    winner.has_color(color).then_some([
-                        (loser_a, MatchPerformance::WIN),
-                        (loser_b, MatchPerformance::WIN),
-                        (loser_c, MatchPerformance::WIN),
-                    ]),
-                    loser_a.has_color(color).then_some([
-                        (winner, MatchPerformance::LOSS),
-                        (loser_b, MatchPerformance::DRAW),
-                        (loser_c, MatchPerformance::DRAW),
-                    ]),
-                    loser_b.has_color(color).then_some([
-                        (winner, MatchPerformance::LOSS),
-                        (loser_a, MatchPerformance::DRAW),
-                        (loser_c, MatchPerformance::DRAW),
-                    ]),
-                    loser_c.has_color(color).then_some([
-                        (winner, MatchPerformance::LOSS),
-                        (loser_a, MatchPerformance::DRAW),
-                        (loser_b, MatchPerformance::DRAW),
-                    ])
-                )
-                .flatten()
+                game_match_perfs(game, |id| {
+                    let Some(info) = self.tourn.get_player_info(&id) else {
+                        return false;
+                    };
+                    info.color_identity().has_color(color)
+                })
+            })
+            .filter_map(|(id, perf)| {
+                Some((self.tourn.get_player_info(&id)?.color_identity(), perf))
             })
             .flat_map(|(identity, perf)| identity.colors().map(move |color| (color, perf)))
             .into_grouping_map()
