4c902ba4217c299b80b8b3f73d299e9a1b11d762
Thomas Kwashnak <thomaskwashnak@gmail.com>
2026-05-25 12:31:26 -0400

    add spacing between tabs

--- app/src/components/tab_bar.rs
+++ app/src/components/tab_bar.rs
@@ -16,29 +16,32 @@ where
     Sel: Fn(Val) -> Msg + 'a,
     Msg: Clone + 'a,
 {
-    container(row(tabs.into_iter().map(|col| {
-        let is_selected = &col == selected;
-        Button::new(text(format!("{col}")))
-            .width(Length::Fill)
-            .style(move |theme: &Theme, status: Status| {
-                let palette = theme.extended_palette();
+    container(
+        row(tabs.into_iter().map(|col| {
+            let is_selected = &col == selected;
+            Button::new(text(format!("{col}")))
+                .width(Length::Fill)
+                .style(move |theme: &Theme, status: Status| {
+                    let palette = theme.extended_palette();
 
-                Style {
-                    text_color: palette.primary.base.text,
-                    background: Some(Background::Color({
-                        if is_selected {
-                            palette.primary.strong.color.scale_alpha(0.9)
-                        } else if matches!(status, Status::Hovered) {
-                            palette.primary.strong.color
-                        } else {
-                            palette.primary.base.color
-                        }
-                    })),
-                    ..Style::default()
-                }
-            })
-            .on_press(on_select(col))
-            .into()
-    })))
+                    Style {
+                        text_color: palette.primary.base.text,
+                        background: Some(Background::Color({
+                            if is_selected {
+                                palette.primary.strong.color.scale_alpha(0.9)
+                            } else if matches!(status, Status::Hovered) {
+                                palette.primary.strong.color
+                            } else {
+                                palette.primary.base.color
+                            }
+                        })),
+                        ..Style::default()
+                    }
+                })
+                .on_press(on_select(col))
+                .into()
+        }))
+        .spacing(1),
+    )
     .width(Length::Fill)
 }
