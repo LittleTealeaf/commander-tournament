c365b303a536ab63e9651b44d014377526f70569
Thomas Kwashnak <thomaskwashnak@gmail.com>
2026-07-05 10:04:26 -0400

    chore: menu items are subtle

--- app/src/app/menu.rs
+++ app/src/app/menu.rs
@@ -2,8 +2,9 @@ use std::path::PathBuf;
 
 use iced::{
     Length,
-    widget::{button, row, space},
+    widget::{button, row, rule, space},
 };
+use iced_aw::widget::InnerBounds::Padding;
 use nerd_font_symbols::md::MD_COG;
 
 use crate::{
@@ -46,10 +47,12 @@ impl ComponentView for Menu {
         Self: 'a;
     fn view<'a>(&'a self, context: Self::ViewContext<'a>) -> iced::Element<'a, Self::Message> {
         row![
-            button("New").on_press(MenuMsg::New),
-            button("Open").on_press(MenuMsg::Open),
-            button("SaveAs").on_press(MenuMsg::SaveAs),
-            button("Save").on_press_maybe(context.is_some().then_some(MenuMsg::Save)),
+            button("New").on_press(MenuMsg::New).style(button::subtle),
+            button("Open").on_press(MenuMsg::Open).style(button::subtle),
+            button("SaveAs").on_press(MenuMsg::SaveAs).style(button::subtle),
+            button("Save")
+                .on_press_maybe(context.is_some().then_some(MenuMsg::Save))
+                .style(button::subtle),
             space().width(Length::Fill),
             button(MD_COG).on_press(MenuMsg::OpenGameConfig),
         ]
