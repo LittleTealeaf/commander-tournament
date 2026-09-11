0ac4158340ad9068a367a76885f3166a38de2587
Thomas Kwashnak <thomaskwashnak@gmail.com>
2026-09-11 19:57:33 -0400

    feat: Add Winstreak selection for next games

--- edh_tourn/src/tournament/next_game.rs
+++ edh_tourn/src/tournament/next_game.rs
@@ -1,9 +1,9 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use crate::{
     error::TournamentError,
     game::{matchup::Matchup, next_mode::NextPlayerMode},
-    player::PlayerId,
+    player::{PlayerId, RegisteredPlayer},
     tournament::Tournament,
 };
 
@@ -46,6 +46,59 @@ impl Tournament {
                     .min_by_key(|player| (player.stats().games(), player.id()))?
                     .id(),
             ),
+
+            NextPlayerMode::WinStreak => {
+                struct Streak<'a> {
+                    player: RegisteredPlayer<'a>,
+                    count: u32,
+                    elo_change: f64,
+                }
+                let mut finished_streaks = HashMap::new();
+
+                let mut player_streaks = players
+                    .map(|player| {
+                        (
+                            player.id(),
+                            Streak {
+                                player,
+                                count: 0,
+                                elo_change: player.stats().elo(),
+                            },
+                        )
+                    })
+                    .collect::<HashMap<_, _>>();
+
+                for game in self.games().iter().rev() {
+                    let winner = game.winner();
+                    if let Some(entry) = player_streaks.get_mut(&winner)
+                        && let Some(player) = game.get_player(winner)
+                    {
+                        entry.count += 1;
+                        entry.elo_change += player.elo_win();
+                    }
+                    for player in game.losers() {
+                        if let Some(entry) = player_streaks.remove(&player) {
+                            finished_streaks.insert(player, entry);
+                        }
+                    }
+                    if player_streaks.is_empty() {
+                        break;
+                    }
+                }
+
+                Some(
+                    finished_streaks
+                        .into_iter()
+                        .max_by(|(left_id, left_streak), (right_id, right_streak)| {
+                            left_streak
+                                .count
+                                .cmp(&right_streak.count)
+                                .then_with(|| left_streak.elo_change.total_cmp(&right_streak.elo_change))
+                                .then_with(|| left_id.cmp(right_id))
+                        })?
+                        .0,
+                )
+            }
         }
     }
 }
